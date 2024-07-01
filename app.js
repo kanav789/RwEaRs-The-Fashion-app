f438b1e987e61a2a394096c59f9c7db8cce729b6
kanav789 <kkanav73@gmamil.com>
2024-07-01 20:15:56 +0530

    change some connection

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("connected");
   })
